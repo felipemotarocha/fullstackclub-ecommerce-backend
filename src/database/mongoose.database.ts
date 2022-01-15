25685f38c333d3c82cb02b457667995227de15e8
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-15 20:36:06 -0300

    chore: add husky and lint-staged setup

--- src/database/mongoose.database.ts
+++ src/database/mongoose.database.ts
@@ -1,16 +1,16 @@
-import * as mongoose from "mongoose";
+import * as mongoose from 'mongoose'
 
 const connectToDatabase = async () => {
   await mongoose.connect(
     `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fscecommerce.ncg0p.mongodb.net/fscEcommerce?retryWrites=true&w=majority`,
     (error) => {
       if (error) {
-        return console.log(`Could not connect to MongoDB: ${error.message}`);
+        return console.log(`Could not connect to MongoDB: ${error.message}`)
       }
 
-      return console.log("Connected to MongoDB!");
+      return console.log('Connected to MongoDB!')
     }
-  );
-};
+  )
+}
 
-export default connectToDatabase;
+export default connectToDatabase
