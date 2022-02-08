3fb6dbcb0b463459420485d15fb1a944b4c1c35f
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-08 07:31:32 -0300

    feat: add delete method and typing to CRUD methods

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -8,25 +8,28 @@ export interface ProductRepositoryAbstract {
   getOne: (id: string) => Promise<Product> | null
   getAll: () => Promise<Product[]>
   update: (id: string, updateProductDto: UpdateProductDto) => Promise<Product>
+  delete: (id: string) => Promise<Product>
 }
 
 export class MongoProductRepository implements ProductRepositoryAbstract {
   async create(createProductDto: CreateProductDto) {
     const product = await ProductModel.create(createProductDto)
 
-    return MongooseHelper.map(product.toJSON())
+    return MongooseHelper.map<Product>(product.toJSON())
   }
 
   async getOne(id: string) {
     const product = await ProductModel.findById(id)
 
-    return MongooseHelper.map(product.toJSON())
+    return MongooseHelper.map<Product>(product.toJSON())
   }
 
   async getAll() {
     const products = await ProductModel.find({})
 
-    return products.map((product) => MongooseHelper.map(product.toJSON()))
+    return products.map((product) =>
+      MongooseHelper.map<Product>(product.toJSON())
+    )
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
@@ -34,6 +37,12 @@ export class MongoProductRepository implements ProductRepositoryAbstract {
       new: true
     })
 
-    return MongooseHelper.map(product.toJSON())
+    return MongooseHelper.map<Product>(product.toJSON())
+  }
+
+  async delete(id: string) {
+    const product = await ProductModel.findByIdAndDelete(id)
+
+    return MongooseHelper.map<Product>(product)
   }
 }
