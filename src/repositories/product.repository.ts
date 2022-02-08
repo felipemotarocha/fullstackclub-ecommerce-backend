9ae497c93b9536e8cde0960cf0420a2f0380ab00
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-08 07:25:41 -0300

    feat: add ProductRepository

---
+++ src/repositories/product.repository.ts
@@ -0,0 +1,39 @@
+import { CreateProductDto, UpdateProductDto } from '../dtos/product.dtos'
+import Product from '../entities/product.entity'
+import MongooseHelper from '../helpers/mongoose.helper'
+import ProductModel from '../models/product.model'
+
+export interface ProductRepositoryAbstract {
+  create: (createProductDto: CreateProductDto) => Promise<Product>
+  getOne: (id: string) => Promise<Product> | null
+  getAll: () => Promise<Product[]>
+  update: (id: string, updateProductDto: UpdateProductDto) => Promise<Product>
+}
+
+export class MongoProductRepository implements ProductRepositoryAbstract {
+  async create(createProductDto: CreateProductDto) {
+    const product = await ProductModel.create(createProductDto)
+
+    return MongooseHelper.map(product.toJSON())
+  }
+
+  async getOne(id: string) {
+    const product = await ProductModel.findById(id)
+
+    return MongooseHelper.map(product.toJSON())
+  }
+
+  async getAll() {
+    const products = await ProductModel.find({})
+
+    return products.map((product) => MongooseHelper.map(product.toJSON()))
+  }
+
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await ProductModel.findByIdAndUpdate(id, updateProductDto, {
+      new: true
+    })
+
+    return MongooseHelper.map(product.toJSON())
+  }
+}
