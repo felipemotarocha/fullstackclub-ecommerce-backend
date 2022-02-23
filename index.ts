78234eb087eb9cc30f0300ed0b538c18d4d1d4c9
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-23 08:45:55 -0300

    feat: add Category Router to our application

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import * as express from 'express'
 import * as cors from 'cors'
 
 import productRouter from './src/routes/product.routes'
+import categoryRouter from './src/routes/category.routes'
 
 import connectToDatabase from './src/database/mongoose.database'
 
@@ -16,6 +17,7 @@ app.use(express.json())
 connectToDatabase()
 
 app.use('/api/product', productRouter)
+app.use('/api/category', categoryRouter)
 
 const port = process.env.PORT || 8000
 app.listen(port, () => console.log(`listening on port ${port}!`))
