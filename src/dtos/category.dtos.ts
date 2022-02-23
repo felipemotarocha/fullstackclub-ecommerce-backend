96cfea3062fd966e911a1aaa941595261457294d
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-23 08:04:24 -0300

    feat: add Category dtos

---
+++ src/dtos/category.dtos.ts
@@ -0,0 +1,11 @@
+export interface CreateCategoryDto {
+  name: string
+  imageUrl: string
+  products: string[]
+}
+
+export interface UpdateCategoryDto {
+  name: string
+  imageUrl: string
+  products: string[]
+}
