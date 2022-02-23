ed9131799d6e712b52476c5076b9786353205c0f
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-23 06:56:37 -0300

    fix: add params to Product Routes

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -6,10 +6,12 @@ const router = Router()
 
 router.get('/', adaptRoute(makeProductController(), 'getAll'))
 
+router.get('/:id', adaptRoute(makeProductController(), 'getOne'))
+
 router.post('/', adaptRoute(makeProductController(), 'create'))
 
-router.patch('/', adaptRoute(makeProductController(), 'update'))
+router.patch('/:id', adaptRoute(makeProductController(), 'update'))
 
-router.delete('/', adaptRoute(makeProductController(), 'delete'))
+router.delete('/:id', adaptRoute(makeProductController(), 'delete'))
 
 export default router
